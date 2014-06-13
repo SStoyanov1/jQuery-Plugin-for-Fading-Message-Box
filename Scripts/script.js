e496b7e3433232dfe475fde271476b6bad34ea9c
SStoyanov1 <hehormajla@gmail.com>
2014-06-13 14:51:37 +0300

    Commit

---
+++ Scripts/script.js
@@ -0,0 +1,36 @@
+﻿(function ($) {
+    $.fn.messageBox = function () {
+        var $this = $(this);
+
+        return {
+            success : function(message){
+                $this.each(function () {
+                    $this.css('background-color', 'green');
+                    $this.animate({ 'opacity': 1 }, 1000);
+                    $(this).html(message);
+                    setInterval(function () { $this.animate({ 'opacity': 0 }, 1000); }, 3000);
+                });
+                return $this;
+            },
+            error : function(message){
+                $this.each(function () {
+                    $this.css('background-color', 'red');
+                    $this.animate({ 'opacity': 1 }, 1000);
+                    $(this).html(message);
+                    setInterval(function () { $this.animate({ 'opacity': 0 }, 1000); }, 3000);
+                });
+                return $this;
+            }
+        };
+    }
+}(jQuery));
+
+var onSuccessButton = $('#on-success-btn').on('click', function () {
+    var msgBox = $('#message-box').messageBox();
+    msgBox.success('Success message');
+});
+
+var onErrorButton = $('#on-error-btn').on('click', function () {
+    var msgBox = $('#message-box').messageBox();
+    msgBox.error('Error message');
+});
